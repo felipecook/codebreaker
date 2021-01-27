79f3c66eddf1b4e70f2b2b6787b5ab4144abb071
Felipe Cook <felipepcook@gmail.com>
2021-01-27 11:15:54 -0700

    Updated Code object with constructor

--- src/com/felipecook/codebreaker/model/Code.java
+++ src/com/felipecook/codebreaker/model/Code.java
@@ -1,5 +1,21 @@
 package com.felipecook.codebreaker.model;
 
+import java.util.Random;
+
 public class Code {
+
+  private final char[] secret;
+
+  public Code(String pool, int length,  Random rng ) {
+
+    secret = new char[length];
+
+    for (int i = 0; i < secret.length; i++) {
+      secret[i] = pool.charAt(rng.nextInt(pool.length()));
+    }
+  }
+
+   
+
 
 }
